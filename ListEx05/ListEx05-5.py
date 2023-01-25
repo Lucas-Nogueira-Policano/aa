f7eb44ed24b6c8a29193e47297dedaf7eccdf1aa
Lucas_Policano <lucas1336policano@gmail.com>
2023-01-25 20:39:43 -0300

    teste5

---
+++ ListEx05/ListEx05-5.py
@@ -0,0 +1,18 @@
+a = int(input())
+V =[]
+d =[]
+if a%2 != 0:
+    for i in range(a):
+        V.append(int(input()))
+    for j in range(a):
+        c = a - j
+        c -= 1
+        d.append(V[c])
+    for k in range(len(V)):
+        if V[k] >= d[k]:
+            V[k] = d[k]
+            e = a - 1
+            V[e-k] = d[e-k]
+        else:
+            break
+print (V)
